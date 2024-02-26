521c7f2f137a3db02febcc312a838e619c6fabab
jingzouzou <827088092@qq.com>
2024-02-26 16:28:07 +0800

    feat: create content script to copy storages

---
+++ contentScript.js
@@ -0,0 +1,54 @@
+// content_script.js
+
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+    console.log("Message received in content script:", message);
+    
+    // 可以在这里对消息进行处理
+    if (message.name === "from_pupop_get_storages") {
+        
+        const allLocalStorage = {};
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            const value = localStorage.getItem(key);
+            allLocalStorage[key] = value;
+        }
+
+        // 获取所有sessionStorage键值对
+        const allSessionStorage = {};
+        for (let i = 0; i < sessionStorage.length; i++) {
+            const key = sessionStorage.key(i);
+            const value = sessionStorage.getItem(key);
+            allSessionStorage[key] = value;
+        }
+
+        sendResponse({
+            local: allLocalStorage,
+            session: allSessionStorage,
+        })
+    }
+
+    if (message.name === "from_pupop_send_local_storage") {
+        console.log('from_pupop_send_local_storage', message.value);
+        for (const key in message.value) {
+            if (Object.hasOwnProperty.call(message.value, key)) {
+                const element = message.value[key];
+                localStorage.setItem(key, element)
+            }
+        }
+    }
+
+    if (message.name === "from_pupop_send_session_storage") {
+        console.log('from_pupop_send_session_storage', message.value);
+
+        for (const key in message.value) {
+            if (Object.hasOwnProperty.call(message.value, key)) {
+                const element = message.value[key];
+                sessionStorage.setItem(key, element)
+            }
+        }
+    }
+
+    // 如果需要，可以向popup.js发送回复消息
+    // sendResponse({ received: true });
+});
